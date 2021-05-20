c3b1eae6275b5eada2358fdbb5dc2d09278b5c9b
Flavio Lucas Fernandes <flaviolukfer@gmail.com>
2021-05-19 23:06:55 -0300

    Initial commit
    Initial commit

---
+++ bin/ddm_dart_project.dart
@@ -0,0 +1,5 @@
+import 'package:ddm_dart_project/ddm_dart_project.dart' as ddm_dart_project;
+
+void main(List<String> arguments) {
+  print('Hello world: ${ddm_dart_project.calculate()}!');
+}
---
+++ test/ddm_dart_project_test.dart
@@ -0,0 +1,8 @@
+import 'package:ddm_dart_project/ddm_dart_project.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
